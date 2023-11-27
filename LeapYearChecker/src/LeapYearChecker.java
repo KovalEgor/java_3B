661aaae43d55242e48e2912ae6da0f13fe11e174
KovalEgor <127386678+KovalEgor@users.noreply.github.com>
2023-11-27 23:46:06 +0900

    Update LeapYearChecker.java

--- LeapYearChecker/src/LeapYearChecker.java
+++ LeapYearChecker/src/LeapYearChecker.java
@@ -8,20 +8,20 @@ import java.util.Scanner;
 
 public class LeapYearChecker {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int year;
+        Scanner scanner = new Scanner(System.in);// Создание объекта класса Scanner для чтения данных с консоли
+        int year;// Объявление переменной для хранения года
 
         try {
-            System.out.print("Введите год: ");
-            year = scanner.nextInt();
-        } catch (Exception e) {
+            System.out.print("Введите год: "); // Вывод на экран приглашения для ввода года
+            year = scanner.nextInt();// Чтение года с консоли и сохранение в переменную year
+        } catch (Exception e) {// Обработка исключения в случае ошибки ввода данных
             System.out.println("Ошибка ввода данных!");
             return;
         }
         if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-            System.out.println(year + " - високосный год");
+            System.out.println(year + " - високосный год");// Вывод на экран сообщения о том, что год високосный
         } else {
-            System.out.println(year + " - невисокосный год");
+            System.out.println(year + " - невисокосный год");// Вывод на экран сообщения о том, что год невисокосный 
         }
     }
 }
